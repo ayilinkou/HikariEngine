a7c981114d4840e8050fa977299730d4ebbc54eb
ayilinkou <66181314+ayilinkou@users.noreply.github.com>
2026-05-01 20:36:27 +0100

    Add symlink to compile_commands.json in root folder when building with build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cmake --workflow --preset ninja-debug-linux
+cmake --workflow --preset ninja-debug-linux && \
+ln -sf build/ninja-debug-linux/compile_commands.json compile_commands.json
