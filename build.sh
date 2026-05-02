ce68688642dcb5cf5979f466141489bb6a543134
ayilinkou <66181314+ayilinkou@users.noreply.github.com>
2026-05-02 14:40:00 +0100

    Added address sanitizer and undefined behaviour sanitizer build preset

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cmake --workflow --preset ninja-debug-linux && \
-ln -sf build/ninja-debug-linux/compile_commands.json compile_commands.json
+cmake --workflow --preset ninja-asan-linux && \
+ln -sf build/ninja-asan-linux/compile_commands.json compile_commands.json
